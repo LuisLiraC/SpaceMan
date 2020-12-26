e980c7e42708888912116cc5e82f249bde6babd9
LuisLiraC <luis_ssbm@hotmail.com>
2020-12-26 01:13:49 -0600

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Player movement variables
+    [SerializeField]
+    private float jumpForce = 6f;
+
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Jump()
+    {
+        
+    }
+}
